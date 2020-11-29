04238ee41651f3d95d827df944c063e346857125
voicefromthesky <andrewdoverin@gmail.com>
2020-11-29 05:16:54 +0200

    first commit

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Fibonachi (int F, int x = 0,int y = 1)
+        {
+            
+            
+            Console.Write(x +" "+ y+" ");
+             
+            if (F>1)
+            {
+                F--;
+                x = y;
+                y = x + y;
+                Fibonachi(F);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            //    // Fibonachi with for//
+            //    Console.WriteLine("How much of Fibonachi do you want?");
+            //    int F = int.Parse(Console.ReadLine()) ;
+            //    int x = 0;
+            //    int y = 1;
+            //    int result;
+            //    Console.Write(x+ " " + y + " ");
+            //    for (int i = 0; i < F; i++)
+            //    {
+            //        result = x + y;
+            //        Console.Write(y+ " ");
+            //        x = y;
+            //        y = result;
+            //    }
+
+            Fibonachi(8);
+
+
+        }
+
+    }
+}
